7f5009297db5187a36ecb89a27f447cfc0c9e487
unknown <holmes@ya.ru>
2021-08-13 10:54:02 +0300

    before add db

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Chat from './components/Chat';
 import ChatList from './components/ChatList';
 import Profile from './components/Profile';
@@ -9,52 +9,34 @@ import { Login } from './components/Login';
 
 export default function Router() {
   return (
-    <React.Fragment>
-      <header>
-        <ul className="header-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/chats">Chats</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-        </ul>
-      </header>
-      <Switch>
-        <Route path="/" exact>
-          <h3>Home page</h3>
-        </Route>
+    <Switch>
+      <Route path="/" exact>
+        <h3>Home page</h3>
+      </Route>
 
-        <PrivateRoute path="/chats" exact>
-          <ChatList />
-        </PrivateRoute>
+      <PrivateRoute path="/chats" exact>
+        <ChatList />
+      </PrivateRoute>
 
-        <PrivateRoute path="/chats/:chatId">
-          <Chat />
-        </PrivateRoute>
+      <PrivateRoute path="/chats/:chatId">
+        <Chat />
+      </PrivateRoute>
 
-        <PrivateRoute path="/profile">
-          <Profile />
-        </PrivateRoute>
+      <PrivateRoute path="/profile">
+        <Profile />
+      </PrivateRoute>
 
-        <Route path="/news">
-          <News />
-        </Route>
+      <Route path="/news">
+        <News />
+      </Route>
 
-        <Route path="/login">
-          <Login />
-        </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
 
-        <Route>
-          Page not found
-        </Route>
-      </Switch>
-    </React.Fragment>
+      <Route>
+        Page not found
+      </Route>
+    </Switch>
   );
 }
